6ba1f677739063cf8e9691876054031e2b01cfd1
atakan18 <atakankasbasi@gmail.com>
2023-03-04 16:24:40 +0300

    #55

--- DataAccesslayer/EntityFramework/EfCommentDal.cs
+++ DataAccesslayer/EntityFramework/EfCommentDal.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccesslayer.Abstract;
+using DataAccesslayer.Concrete;
 using DataAccesslayer.Repository;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,12 @@ namespace DataAccesslayer.EntityFramework
 {
     public class EfCommentDal : GenericRepository<Comment>, ICommentDal
     {
+        public List<Comment> GetListCommentWithDestination()
+        {
+            using(var c = new Context())
+            {
+                return c.Comments.Include(x => x.Destination).ToList();
+            }
+        }
     }
 }
